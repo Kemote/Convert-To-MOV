a722adce381ab043b89284818265a0f81a3197d9
Kemote <tomek.szymski@gmail.com>
2019-08-08 17:53:08 +0200

    Add files via upload

---
+++ CTM.bat
@@ -0,0 +1,2 @@
+@echo off
+C:\ConvertToMOV.py %1
---
+++ ConvertToMOV.py
@@ -0,0 +1,22 @@
+import sys
+import re
+
+print("Convert to MOV by Kemote")
+
+tmp = ""
+file = sys.argv[1].split("\\")[-1]
+path = sys.argv[1].split("\\")[0:-1]
+format = sys.argv[1].split(".")[-1]
+for i in path:
+	tmp +=(i + "\\")
+path = tmp
+index = re.findall("\d+", file)[-1]
+index = "%0" + str(len(index)) + "d"
+file = file[0:-(len(index) + len(format) +1)]
+
+print(index)
+print(format)	
+print(file)
+print(path)
+
+input('preess jakis przycisk')
